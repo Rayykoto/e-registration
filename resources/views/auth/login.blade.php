1e70654888ec84dff0805076221c2bac771f473b
Hamdani <hamdani9304@gmail.com>
2022-03-04 09:02:31 +0700

    Update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,83 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Wynacom | Log in </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wynacom | Log in </title>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-<link rel="stylesheet" href="{{ asset('assets/') }}/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/') }}/plugins/fontawesome-free/css/all.min.css">
 
-<link rel="stylesheet" href="{{ asset('assets/') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
 
-<link rel="stylesheet" href="{{ asset('assets/') }}/dist/css/adminlte.min.css?v=3.2.0">
-<script nonce="978ca9e0-651f-44e6-8423-4f156e8255db">(function(w,d){!function(a,e,t,r,z){a.zarazData=a.zarazData||{},a.zarazData.executed=[],a.zarazData.tracks=[],a.zaraz={deferred:[]};var s=e.getElementsByTagName("title")[0];s&&(a.zarazData.t=e.getElementsByTagName("title")[0].text),a.zarazData.w=a.screen.width,a.zarazData.h=a.screen.height,a.zarazData.j=a.innerHeight,a.zarazData.e=a.innerWidth,a.zarazData.l=a.location.href,a.zarazData.r=e.referrer,a.zarazData.k=a.screen.colorDepth,a.zarazData.n=e.characterSet,a.zarazData.o=(new Date).getTimezoneOffset(),a.dataLayer=a.dataLayer||[],a.zaraz.track=(e,t)=>{for(key in a.zarazData.tracks.push(e),t)a.zarazData["z_"+key]=t[key]},a.zaraz._preSet=[],a.zaraz.set=(e,t,r)=>{a.zarazData["z_"+e]=t,a.zaraz._preSet.push([e,t,r])},a.dataLayer.push({"zaraz.start":(new Date).getTime()}),a.addEventListener("DOMContentLoaded",(()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r);z.defer=!0,z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData))),t.parentNode.insertBefore(z,t)}))}(w,d,0,"script");})(window,document);</script></head>
+    <link rel="stylesheet" href="{{ asset('assets/') }}/dist/css/adminlte.min.css?v=3.2.0">
+    <script nonce="978ca9e0-651f-44e6-8423-4f156e8255db">
+        (function(w,d){!function(a,e,t,r,z){a.zarazData=a.zarazData||{},a.zarazData.executed=[],a.zarazData.tracks=[],a.zaraz={deferred:[]};var s=e.getElementsByTagName("title")[0];s&&(a.zarazData.t=e.getElementsByTagName("title")[0].text),a.zarazData.w=a.screen.width,a.zarazData.h=a.screen.height,a.zarazData.j=a.innerHeight,a.zarazData.e=a.innerWidth,a.zarazData.l=a.location.href,a.zarazData.r=e.referrer,a.zarazData.k=a.screen.colorDepth,a.zarazData.n=e.characterSet,a.zarazData.o=(new Date).getTimezoneOffset(),a.dataLayer=a.dataLayer||[],a.zaraz.track=(e,t)=>{for(key in a.zarazData.tracks.push(e),t)a.zarazData["z_"+key]=t[key]},a.zaraz._preSet=[],a.zaraz.set=(e,t,r)=>{a.zarazData["z_"+e]=t,a.zaraz._preSet.push([e,t,r])},a.dataLayer.push({"zaraz.start":(new Date).getTime()}),a.addEventListener("DOMContentLoaded",(()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r);z.defer=!0,z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData))),t.parentNode.insertBefore(z,t)}))}(w,d,0,"script");})(window,document);
+    </script>
+</head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
+    <div class="login-box">
 
-<div class="card card-outline card-primary">
-<div class="card-header text-center">
-<a href="{{ asset('assets/') }}/index2.html" class="h1"><b>Login</b></a>
-</div>
-<div class="card-body">
-<p class="login-box-msg">Login Your Session</p>
-<form action="{{ asset('assets/') }}/index3.html" method="post">
-<div class="input-group mb-3">
-<input type="email" class="form-control" placeholder="Email">
-<div class="input-group-append">
-<div class="input-group-text">
-<span class="fas fa-envelope"></span>
-</div>
-</div>
-</div>
-<div class="input-group mb-3">
-<input type="password" class="form-control" placeholder="Password">
-<div class="input-group-append">
-<div class="input-group-text">
-<span class="fas fa-lock"></span>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col-8">
-<div class="icheck-primary">
-<input type="checkbox" id="remember">
-<label for="remember">
-Remember Me
-</label>
-</div>
-</div>
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="{{ asset('assets/') }}/index2.html" class="h1"><b>Login</b></a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Login Your Session</p>
+                <form method="POST" action="{{ url('/postlogin') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" name="email" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
 
-<div class="col-4">
-<button type="submit" class="btn btn-primary btn-block">Sign In</button>
-</div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
 
-</div>
-</form>
-<div class="social-auth-links text-center mt-2 mb-3">
-<a href="{{ __('register') }}" class="btn btn-block btn-primary">
-<i class="fas fa fa-user"> </i> Buat Aku Klik Ini
-</a>
-
-
-</div>
-
-</div>
+                    </div>
+                </form>
+                <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="{{ __('register') }}" class="btn btn-block btn-primary">
+                        <i class="fas fa fa-user"> </i> Buat Aku Klik Ini
+                    </a>
 
 
-<script src="{{ asset('assets/') }}/plugins/jquery/jquery.min.js"></script>
+                </div>
 
-<script src="{{ asset('assets/') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+            </div>
 
-<script src="{{ asset('assets/') }}/dist/js/adminlte.min.js?v=3.2.0"></script>
+
+            <script src="{{ asset('assets/') }}/plugins/jquery/jquery.min.js"></script>
+
+            <script src="{{ asset('assets/') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+            <script src="{{ asset('assets/') }}/dist/js/adminlte.min.js?v=3.2.0"></script>
 </body>
+
 </html>
