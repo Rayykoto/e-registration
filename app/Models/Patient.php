801fbb9b833e838ffba227ab97eb38e0ba01908e
Hamdani <hamdani9304@gmail.com>
2022-03-07 02:33:22 +0700

    Penambahan model patient

---
+++ app/Models/Patient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ro_patient';
+    protected $guarded = [];
+}
